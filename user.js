627de44aac5bf2e6f31c68a336a7e2e2ab3d1bfa
Shivanshu  Kaushik <shivanshukaushik.23o9@gmail.com>
2024-12-20 18:00:44 +0530

    Add files via upload

---
+++ user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// User Schema
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String, // Combine firstname and lastname to create a username
+        required: true
+    }
+});
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
